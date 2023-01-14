10860a28b3ba8e31ec43ab3c20c5621f7e5a3a2b
AkaHeez <118094954+AkaHeez@users.noreply.github.com>
2023-01-14 18:32:53 -0500

    Update cheat.py

--- cheat.py
+++ cheat.py
@@ -1,10 +1,8 @@
-code = input("what is the code? ").upper()
-PATH = f"https://jklm.fun/{code}"
-
-
-USER = "Username here, anything works"
 FILEPATH = r"Right click the words.txt and then copy as path and paste it in here"
-AMOUNTOFWORDS = 15 #This is the amount of words it prints out for u on the side
+AMOUNT = 15 #amount of words you want it to type out when soft cheating
+
+
+cheat = int(input("1) Soft Cheat\n2) Hard Cheat\n"))
 
 
 
@@ -15,17 +13,33 @@ from os import system as sys
 
 
 
-def promptCheck(words, prompt):
+def promptCheck(prompt):
     sys("cls")
     counter = 0
     print(prompt)
     for word in words:
         if prompt in word:
-            if counter < AMOUNTOFWORDS:
+            if counter < AMOUNT:
                 print(word)
                 counter += 1
             else:
                 return prompt
+
+
+def promptAI(prompt):
+    for word in words:
+        if prompt in word and word not in usedWords:
+            usedWords.append(word)
+            return word
+
+
+
+
+
+
+
+
+
 
 
 def main():
@@ -35,35 +49,58 @@ def main():
     from selenium.webdriver.common.by import By
     from selenium.webdriver.support.ui import WebDriverWait
     from selenium.webdriver.support import expected_conditions as EC
-    from selenium.common.exceptions import NoSuchElementException
 
 
 
     #open the list of words 0-7 letters of bombparty words
+    global words, usedWords
     words = open(FILEPATH, "r").read().split()
 
     #waits in between to make sure that the browser load completly
     driver = webdriver.Chrome()
-    html = driver.get(PATH)
+    html = driver.get("https://jklm.fun")
     wait = WebDriverWait(driver, 10)
-    wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[3]/form/div[2]/input")))
-    sleep(1)
-    driver.find_element(By.XPATH,value="/html/body/div[2]/div[3]/form/div[2]/input").send_keys(USER)
-    driver.find_element(By.XPATH,value="/html/body/div[2]/div[3]/form/div[2]/button").click()
+
+
     #switching to see the iframe to see the information of text and user's playing
-    sleep(10)
-    driver.switch_to.frame(0)
-    wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[3]/div[1]/div[1]/button")))
-    sleep(1)
-    driver.find_element(By.XPATH,value="/html/body/div[2]/div[3]/div[1]/div[1]/button").click()
-    #looping the process of if the user is me and if it is, it prints out the words usuable in the console, up to 15
-    previousP = "lol hi"
-    while True:    
-        currentPlayer = driver.find_element(By.XPATH, value='/html/body/div[2]/div[3]/div[2]/div[1]')
+    wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div[5]")))
+    while True:
         sleep(1)
-        if currentPlayer.get_attribute("hidden") == "true":
-            prompt = driver.find_element(By.XPATH, value='/html/body/div[2]/div[2]/div[2]/div[2]/div')
-            if (prompt.text.lower()) != previousP:
-                previousP = promptCheck(words, prompt.text.lower())
+        if driver.current_url.endswith("fun/"):
+            continue
+        break
+    while True:
+        sleep(1)
+        if driver.find_element(By.XPATH, value="/html/body/div[2]/div[3]").get_attribute("hidden") == "true":
+            break
+    wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[4]/div[1]")))
+    sleep(1)
+    driver.switch_to.frame(0)
+
+
+    previousP = "lol hi"
+    usedWords = []
+    if cheat == 2: #Hard cheat
+        while True:    
+            currentPlayer = driver.find_element(By.XPATH, value="/html/body/div[2]/div[3]/div[2]/div[1]")
+            sleep(1)
+            if currentPlayer.get_attribute("hidden") == "true":
+                prompt = driver.find_element(By.XPATH, value="/html/body/div[2]/div[2]/div[2]/div[2]/div")
+                textbox = driver.find_element(By.XPATH,value="/html/body/div[2]/div[3]/div[2]/div[2]/form/input")
+                textbox.send_keys(promptAI(prompt.text.lower()))
+                textbox.submit()
+                
+
+
+    else: #soft cheat
+        while True:    
+            currentPlayer = driver.find_element(By.XPATH, value="/html/body/div[2]/div[3]/div[2]/div[1]")
+            sleep(1)
+            if currentPlayer.get_attribute("hidden") == "true":
+                prompt = driver.find_element(By.XPATH, value="/html/body/div[2]/div[2]/div[2]/div[2]/div")
+                if (prompt.text.lower()) != previousP:
+                    previousP = promptCheck(prompt.text.lower())
+
+
 if __name__ == "__main__":
     main()
